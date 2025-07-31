4c0684ef0d1f744c01f253666e311ab0320b9091
Soumaditya Deb <167998566+Jontybr18211@users.noreply.github.com>
2025-07-31 19:22:15 +0530

    Delete capture.py

--- capture.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-def get_video_capture(src=0, width=640, height=480):
-    cap = cv2.VideoCapture(src)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-    return cap
